04a4aa57aeb72fd89119714221bea6c392553a6e
baneshi <bane.shi@tradeshift.com>
2021-08-13 09:57:22 +0800

    tmp

--- commons-core/src/main/java/com/moensun/commons/core/util/IdUtils.java
+++ commons-core/src/main/java/com/moensun/commons/core/util/IdUtils.java
@@ -3,7 +3,7 @@ package com.moensun.commons.core.util;
 import java.util.Objects;
 
 public class IdUtils {
-    public boolean longIdValid(Long id){
+    public static boolean longIdValid(Long id){
         return Objects.nonNull(id) && !Objects.equals(0L,id);
     }
 }
