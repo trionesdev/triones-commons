5c72e27b9586c2100879400e7d7a3c9bc810bc6c
bane.shi <fengxiaotx@163.com>
2021-08-21 21:49:55 +0800

    init

--- commons-core/src/main/java/com/moensun/commons/core/util/IdentityUtils.java
+++ commons-core/src/main/java/com/moensun/commons/core/util/IdentityUtils.java
@@ -2,11 +2,8 @@ package com.moensun.commons.core.util;
 
 import java.util.Objects;
 
-<<<<<<< HEAD:commons-core/src/main/java/com/moensun/commons/core/util/IdentityUtils.java
 public class IdentityUtils {
-=======
-public class IdUtils {
->>>>>>> origin/master:commons-core/src/main/java/com/moensun/commons/core/util/IdUtils.java
+
     public static boolean longIdValid(Long id){
         return Objects.nonNull(id) && !Objects.equals(0L,id);
     }
