0162ca0e55fbf92c48470168840bc46c5bb6a6bc
bane.shi <fengxiaotx@163.com>
2021-09-20 22:24:20 +0800

    init

---
+++ commons-context/src/main/java/com/moensun/commons/context/operator/OperatorConstants.java
@@ -0,0 +1,7 @@
+package com.moensun.commons.context.operator;
+
+public class OperatorConstants {
+    public static final String OPERATOR_ID = "Operator_Id";
+    public static final String TENANT_ID = "Tenant_Id";
+    public static final String TRACE_ID = "Trace_Id";
+}
