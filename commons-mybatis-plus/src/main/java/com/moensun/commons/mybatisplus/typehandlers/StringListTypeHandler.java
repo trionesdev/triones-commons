03cc3ef65e1a6527f1f7fcd2202ee50927e53c26
bane.shi <fengxiaotx@163.com>
2021-12-12 14:23:04 +0800

    []

---
+++ commons-mybatis-plus/src/main/java/com/moensun/commons/mybatisplus/typehandlers/StringListTypeHandler.java
@@ -0,0 +1,54 @@
+package com.moensun.commons.mybatisplus.typehandlers;
+
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.extension.handlers.AbstractJsonTypeHandler;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+import java.util.List;
+
+@Slf4j
+@MappedTypes({List.class})
+@MappedJdbcTypes(JdbcType.VARCHAR)
+public class StringListTypeHandler extends AbstractJsonTypeHandler<List<String>> {
+
+    private static ObjectMapper OBJECT_MAPPER;
+    private final Class<?> type;
+
+    public StringListTypeHandler(Class<?> type) {
+        if (log.isTraceEnabled()) {
+            log.trace("JacksonTypeHandler(" + type + ")");
+        }
+        Assert.notNull(type, "Type argument cannot be null");
+        this.type = type;
+    }
+
+    public static ObjectMapper getObjectMapper() {
+        if (null == OBJECT_MAPPER) {
+            OBJECT_MAPPER = new ObjectMapper();
+        }
+        return OBJECT_MAPPER;
+    }
+
+    public static void setObjectMapper(ObjectMapper objectMapper) {
+        Assert.notNull(objectMapper, "ObjectMapper should not be null");
+        StringListTypeHandler.OBJECT_MAPPER = objectMapper;
+    }
+
+    @SneakyThrows
+    @Override
+    protected List<String> parse(String json) {
+        return getObjectMapper().readValue(json, new TypeReference<List<String>>() {});
+    }
+
+    @SneakyThrows
+    @Override
+    protected String toJson(List<String> obj) {
+        return getObjectMapper().writeValueAsString(obj);
+    }
+}
