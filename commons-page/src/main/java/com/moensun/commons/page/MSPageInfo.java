054fa0c9ba56b4be90ff4cdf65e45103b645175d
bane.shi <fengxiaotx@163.com>
2022-01-27 00:49:45 +0800

    article column

--- commons-page/src/main/java/com/moensun/commons/page/MSPageInfo.java
+++ commons-page/src/main/java/com/moensun/commons/page/MSPageInfo.java
@@ -1,6 +1,7 @@
 package com.moensun.commons.page;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Data
 @SuperBuilder
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class MSPageInfo<T> implements Serializable {
