314ce2da8182a282720252c72e1dc20d862215ba
bane.shi <fengxiaotx@163.com>
2022-02-04 02:07:49 +0800

    article column

--- commons-core/src/main/java/com/moensun/commons/core/util/HtmlUtils.java
+++ commons-core/src/main/java/com/moensun/commons/core/util/HtmlUtils.java
@@ -2,11 +2,14 @@ package com.moensun.commons.core.util;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.safety.Safelist;
 
 public class HtmlUtils {
 
     private static final Safelist relaxedWhitelist = Safelist.relaxed();
+
+    static Document.OutputSettings relaxedOutputSettings = new Document.OutputSettings().prettyPrint(false);
 
     static {
         relaxedWhitelist.addAttributes(":all", "style");
@@ -14,13 +17,14 @@ public class HtmlUtils {
 
     /**
      * xxs攻击内容去除
+     *
      * @param content
      * @return
      */
-    public static String xxsClean(String content){
-        if(StringUtils.isBlank(content)){
+    public static String xxsClean(String content) {
+        if (StringUtils.isBlank(content)) {
             return content;
         }
-        return Jsoup.clean(content,relaxedWhitelist);
+        return Jsoup.clean(content, "", relaxedWhitelist, relaxedOutputSettings);
     }
 }
