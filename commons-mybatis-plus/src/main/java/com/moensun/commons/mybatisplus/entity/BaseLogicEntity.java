1f7e3a9909a5beb462dd80119f53d547bc73784d
bane.shi <fengxiaotx@163.com>
2022-02-26 21:05:04 +0800

    logic entity

--- commons-mybatis-plus/src/main/java/com/moensun/commons/mybatisplus/entity/BaseLogicEntity.java
+++ commons-mybatis-plus/src/main/java/com/moensun/commons/mybatisplus/entity/BaseLogicEntity.java
@@ -2,6 +2,7 @@ package com.moensun.commons.mybatisplus.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,10 @@ import java.time.Instant;
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
-public class BaseEntity {
+public class BaseLogicEntity {
+    @TableLogic(value = "0",delval = "1")
+    @TableField(value = "is_deleted",fill = FieldFill.INSERT)
+    private Boolean deleted;
     @TableField(fill = FieldFill.INSERT)
     private Instant createdAt;
     @TableField(fill = FieldFill.INSERT)
