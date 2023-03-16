76d82f7802d5604397019c3a1e98580421800534
Bane.Shi <fengxiaotx@163.com>
2023-03-16 14:39:18 +0800

    [develop] page

--- commons-core/src/main/java/com/moensun/commons/core/page/MSPageInfo.java
+++ commons-core/src/main/java/com/moensun/commons/core/page/MSPageInfo.java
@@ -9,6 +9,14 @@ import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MSPageInfo<T> extends PageInfo<T> {
-
+    private static final long serialVersionUID = 7156771088266349472L;
+    private int pageNum;
+    private int pageSize;
+    private long total;
+    private int pages;
+    private List<T> rows = Collections.emptyList();
 }
