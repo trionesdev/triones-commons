deecf5840109e47ae1fcd940c9252247f5314a43
bane.shi <fengxiaotx@163.com>
2023-04-15 15:07:29 +0800

    [develop]

--- commons-security-spring/src/main/java/com/moensun/commons/security/spring/jwt/JwtServerConfigurer.java
+++ commons-security-spring/src/main/java/com/moensun/commons/security/spring/jwt/JwtServerConfigurer.java
@@ -3,15 +3,15 @@ package com.moensun.commons.security.spring.jwt;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextHolderFilter;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
 public class JwtServerConfigurer<H extends HttpSecurityBuilder<H>>
         extends AbstractHttpConfigurer<JwtServerConfigurer<H>, H> {
     private final ApplicationContext context;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public JwtServerConfigurer(ApplicationContext context, JwtAuthenticationFilter jwtAuthenticationFilter){
+    public JwtServerConfigurer(ApplicationContext context, JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.context = context;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
@@ -23,6 +23,6 @@ public class JwtServerConfigurer<H extends HttpSecurityBuilder<H>>
 
     @Override
     public void configure(H builder) {
-        builder.addFilterAfter(jwtAuthenticationFilter, SecurityContextHolderFilter.class);
+        builder.addFilterAfter(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
