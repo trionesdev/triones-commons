02e5e83a24c956b1362ee0caf8f3b08fc0510c6b
bane.shi <fengxiaotx@163.com>
2023-05-12 11:20:14 +0800

    feat: 更新mybatis-plus 的多住户插件，变为继承官方的多租户插件模式

--- commons-mybatis-plus/src/main/java/com/moensun/commons/mybatisplus/plugins/handler/TenantLineMultiHandler.java
+++ commons-mybatis-plus/src/main/java/com/moensun/commons/mybatisplus/plugins/handler/TenantLineMultiHandler.java
@@ -1,50 +1,19 @@
 package com.moensun.commons.mybatisplus.plugins.handler;
 
-import net.sf.jsqlparser.expression.Expression;
-import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import net.sf.jsqlparser.expression.operators.relational.ItemsList;
-import net.sf.jsqlparser.schema.Column;
 
-import java.util.List;
-
-public interface TenantLineMultiHandler {
-
-    Expression getTenantId();
-
-    ItemsList getTenantIds();
-
+/**
+ * 允许同时查询多个租户的多租户
+ */
+public interface TenantLineMultiHandler extends TenantLineHandler {
     /**
-     * 获取租户字段名
-     * <p>
-     * 默认字段名叫: tenant_id
-     *
-     * @return 租户字段名
-     */
-    default String getTenantIdColumn() {
-        return "tenant_id";
-    }
-
-    /**
-     * 根据表名判断是否忽略拼接多租户条件
-     * <p>
-     * 默认都要进行解析并拼接多租户条件
-     *
-     * @param tableName 表名
-     * @return 是否忽略, true:表示忽略，false:需要解析并拼接多租户条件
-     */
-    default boolean ignoreTable(String tableName) {
-        return false;
-    }
-
-    /**
-     * 忽略插入租户字段逻辑
-     *
-     * @param columns        插入字段
-     * @param tenantIdColumn 租户 ID 字段
+     * 是否开启多租户查询 支持查询多个租户数据
+     * 如果不开启，则与普通多租户一样
      * @return
      */
-    default boolean ignoreInsert(List<Column> columns, String tenantIdColumn) {
-        return columns.stream().map(Column::getColumnName).anyMatch(i -> i.equalsIgnoreCase(tenantIdColumn));
-    }
+    boolean enableTenantMulti();
+
+    ItemsList getTenantIds();
 
 }
--- commons-mybatis-plus/src/main/java/com/moensun/commons/mybatisplus/plugins/inner/TenantLineMultiInnerInterceptor.java
+++ commons-mybatis-plus/src/main/java/com/moensun/commons/mybatisplus/plugins/inner/TenantLineMultiInnerInterceptor.java
@@ -1,220 +1,56 @@
 package com.moensun.commons.mybatisplus.plugins.inner;
 
-import com.baomidou.mybatisplus.core.plugins.InterceptorIgnoreHelper;
-import com.baomidou.mybatisplus.core.toolkit.*;
-import com.baomidou.mybatisplus.extension.parser.JsqlParserSupport;
-import com.baomidou.mybatisplus.extension.plugins.inner.BaseMultiTableInnerInterceptor;
+import com.baomidou.mybatisplus.core.toolkit.ClassUtils;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
 import com.baomidou.mybatisplus.extension.toolkit.PropertyMapper;
 import com.moensun.commons.mybatisplus.plugins.handler.TenantLineMultiHandler;
-import lombok.*;
-import net.sf.jsqlparser.expression.*;
-import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
-import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.operators.relational.*;
 import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.schema.Table;
-import net.sf.jsqlparser.statement.delete.Delete;
-import net.sf.jsqlparser.statement.insert.Insert;
-import net.sf.jsqlparser.statement.select.*;
-import net.sf.jsqlparser.statement.update.Update;
-import org.apache.ibatis.executor.Executor;
-import org.apache.ibatis.executor.statement.StatementHandler;
-import org.apache.ibatis.mapping.BoundSql;
-import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.mapping.SqlCommandType;
-import org.apache.ibatis.session.ResultHandler;
-import org.apache.ibatis.session.RowBounds;
+import net.sf.jsqlparser.statement.select.SubSelect;
+import net.sf.jsqlparser.statement.select.WithItem;
 
-import java.sql.Connection;
 import java.util.List;
 import java.util.Properties;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class TenantLineMultiInnerInterceptor extends BaseMultiTableInnerInterceptor implements InnerInterceptor {
+public class TenantLineMultiInnerInterceptor extends TenantLineInnerInterceptor implements InnerInterceptor {
     private TenantLineMultiHandler tenantLineMultiHandler;
 
-    @Override
-    public void beforeQuery(
-            Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) {
-        if (InterceptorIgnoreHelper.willIgnoreTenantLine(ms.getId())) return;
-        PluginUtils.MPBoundSql mpBs = PluginUtils.mpBoundSql(boundSql);
-        mpBs.sql(parserSingle(mpBs.sql(), null));
-    }
-
-    @Override
-    public void beforePrepare(StatementHandler sh, Connection connection, Integer transactionTimeout) {
-        PluginUtils.MPStatementHandler mpSh = PluginUtils.mpStatementHandler(sh);
-        MappedStatement ms = mpSh.mappedStatement();
-        SqlCommandType sct = ms.getSqlCommandType();
-        if (sct == SqlCommandType.INSERT || sct == SqlCommandType.UPDATE || sct == SqlCommandType.DELETE) {
-            if (InterceptorIgnoreHelper.willIgnoreTenantLine(ms.getId())) return;
-            PluginUtils.MPBoundSql mpBs = mpSh.mPBoundSql();
-            mpBs.sql(parserMulti(mpBs.sql(), null));
-        }
-    }
-
-    @Override
-    protected void processSelect(Select select, int index, String sql, Object obj) {
-        String whereSegment = (String) obj;
-        this.processSelectBody(select.getSelectBody(), whereSegment);
-        List<WithItem> withItemsList = select.getWithItemsList();
-        if (!CollectionUtils.isEmpty(withItemsList)) {
-            withItemsList.forEach(withItem -> processSelectBody(withItem, whereSegment));
-        }
-    }
-
-    @Override
-    protected void processInsert(Insert insert, int index, String sql, Object obj) {
-        if (!this.tenantLineMultiHandler.ignoreTable(insert.getTable().getName())) {
-            List<Column> columns = insert.getColumns();
-            if (!CollectionUtils.isEmpty(columns)) {
-                String tenantIdColumn = this.tenantLineMultiHandler.getTenantIdColumn();
-                if (!this.tenantLineMultiHandler.ignoreInsert(columns, tenantIdColumn)) {
-                    columns.add(new Column(tenantIdColumn));
-                    List<Expression> duplicateUpdateColumns = insert.getDuplicateUpdateExpressionList();
-                    if (CollectionUtils.isNotEmpty(duplicateUpdateColumns)) {
-                        EqualsTo equalsTo = new EqualsTo();
-                        equalsTo.setLeftExpression(new StringValue(tenantIdColumn));
-                        equalsTo.setRightExpression(this.tenantLineMultiHandler.getTenantId());
-                        duplicateUpdateColumns.add(equalsTo);
-                    }
-
-                    Select select = insert.getSelect();
-                    if (select != null) {
-                        this.processInsertSelect(select.getSelectBody(), (String)obj);
-                    } else {
-                        if (insert.getItemsList() == null) {
-                            throw ExceptionUtils.mpe("Failed to process multiple-table update, please exclude the tableName or statementId", new Object[0]);
-                        }
-
-                        ItemsList itemsList = insert.getItemsList();
-                        Expression tenantId = this.tenantLineMultiHandler.getTenantId();
-                        if (itemsList instanceof MultiExpressionList) {
-                            ((MultiExpressionList)itemsList).getExpressionLists().forEach((el) -> {
-                                el.getExpressions().add(tenantId);
-                            });
-                        } else {
-                            ((ExpressionList)itemsList).getExpressions().add(tenantId);
-                        }
-                    }
-
-                }
+    public TenantLineMultiInnerInterceptor(TenantLineMultiHandler tenantLineMultiHandler) {
+        super(new TenantLineHandler() {
+            @Override
+            public Expression getTenantId() {
+                return tenantLineMultiHandler.getTenantId();
             }
-        }
-    }
 
-    /**
-     * update 语句处理
-     */
-    @Override
-    protected void processUpdate(Update update, int index, String sql, Object obj) {
-        final Table table = update.getTable();
-        if (tenantLineMultiHandler.ignoreTable(table.getName())) {
-            // 过滤退出执行
-            return;
-        }
-        update.setWhere(this.andExpression(table, update.getWhere()));
-    }
-
-    /**
-     * delete 语句处理
-     */
-    @Override
-    protected void processDelete(Delete delete, int index, String sql, Object obj) {
-        if (tenantLineMultiHandler.ignoreTable(delete.getTable().getName())) {
-            // 过滤退出执行
-            return;
-        }
-        delete.setWhere(this.andExpression(delete.getTable(), delete.getWhere()));
-    }
-
-    /**
-     * delete update 语句 where 处理
-     */
-    protected Expression andExpression(Table table, Expression where) {
-        if (isMultiTenant(tenantLineMultiHandler.getTenantIds())) {
-            //获得where条件表达式
-            InExpression inExpression = new InExpression();
-            inExpression.setLeftExpression(this.getAliasColumn(table));
-            inExpression.setRightItemsList(tenantLineMultiHandler.getTenantIds());
-            if (null != where) {
-                if (where instanceof OrExpression) {
-                    return new AndExpression(inExpression, new Parenthesis(where));
-                } else {
-                    return new AndExpression(inExpression, where);
-                }
+            @Override
+            public String getTenantIdColumn() {
+                return tenantLineMultiHandler.getTenantIdColumn();
             }
-            return inExpression;
-        } else {
-            //获得where条件表达式
-            EqualsTo equalsTo = new EqualsTo();
-            equalsTo.setLeftExpression(this.getAliasColumn(table));
-            equalsTo.setRightExpression(tenantLineMultiHandler.getTenantId());
-            if (null != where) {
-                if (where instanceof OrExpression) {
-                    return new AndExpression(equalsTo, new Parenthesis(where));
-                } else {
-                    return new AndExpression(equalsTo, where);
-                }
+
+            @Override
+            public boolean ignoreTable(String tableName) {
+                return tenantLineMultiHandler.ignoreTable(tableName);
             }
-            return equalsTo;
-        }
-    }
 
-    /**
-     * 处理 insert into select
-     * <p>
-     * 进入这里表示需要 insert 的表启用了多租户,则 select 的表都启动了
-     *
-     * @param selectBody SelectBody
-     */
-    protected void processInsertSelect(SelectBody selectBody, final String whereSegment) {
-        PlainSelect plainSelect = (PlainSelect) selectBody;
-        FromItem fromItem = plainSelect.getFromItem();
-        if (fromItem instanceof Table) {
-            // fixed gitee pulls/141 duplicate update
-            processPlainSelect(plainSelect, whereSegment);
-            appendSelectItem(plainSelect.getSelectItems());
-        } else if (fromItem instanceof SubSelect) {
-            SubSelect subSelect = (SubSelect) fromItem;
-            appendSelectItem(plainSelect.getSelectItems());
-            processInsertSelect(subSelect.getSelectBody(), whereSegment);
-        }
-    }
-
-    /**
-     * 追加 SelectItem
-     *
-     * @param selectItems SelectItem
-     */
-    protected void appendSelectItem(List<SelectItem> selectItems) {
-        if (CollectionUtils.isEmpty(selectItems)) return;
-        if (selectItems.size() == 1) {
-            SelectItem item = selectItems.get(0);
-            if (item instanceof AllColumns || item instanceof AllTableColumns) return;
-        }
-        selectItems.add(new SelectExpressionItem(new Column(tenantLineMultiHandler.getTenantIdColumn())));
-    }
-
-    /**
-     * 租户字段别名设置
-     * <p>tenantId 或 tableAlias.tenantId</p>
-     *
-     * @param table 表对象
-     * @return 字段
-     */
-    protected Column getAliasColumn(Table table) {
-        StringBuilder column = new StringBuilder();
-        if (table.getAlias() != null) {
-            column.append(table.getAlias().getName()).append(StringPool.DOT);
-        }
-        column.append(tenantLineMultiHandler.getTenantIdColumn());
-        return new Column(column.toString());
+            @Override
+            public boolean ignoreInsert(List<Column> columns, String tenantIdColumn) {
+                return tenantLineMultiHandler.ignoreInsert(columns, tenantIdColumn);
+            }
+        });
+        this.tenantLineMultiHandler = tenantLineMultiHandler;
     }
 
     @Override
@@ -224,6 +60,9 @@ public class TenantLineMultiInnerInterceptor extends BaseMultiTableInnerIntercep
 
 
     private boolean isMultiTenant(ItemsList itemsList) {
+        if (!tenantLineMultiHandler.enableTenantMulti()) {
+            return false;
+        }
         if (itemsList instanceof ExpressionList) {
             List<Expression> expressionList = ((ExpressionList) itemsList).getExpressions();
             return CollectionUtils.isNotEmpty(expressionList) && expressionList.size() > 1;
@@ -246,29 +85,9 @@ public class TenantLineMultiInnerInterceptor extends BaseMultiTableInnerIntercep
             return null;
         }
         if (isMultiTenant(tenantLineMultiHandler.getTenantIds())) {
-            InExpression inExpression = new InExpression();
-            inExpression.setLeftExpression(this.getAliasColumn(table));
-            inExpression.setRightItemsList(tenantLineMultiHandler.getTenantIds());
-            if (where == null) {
-                return inExpression;
-            }
-            if (where instanceof OrExpression) {
-                return new AndExpression(new Parenthesis(where), inExpression);
-            } else {
-                return new AndExpression(where, inExpression);
-            }
+            return new InExpression(getAliasColumn(table), tenantLineMultiHandler.getTenantIds());
         } else {
-            EqualsTo equalsTo = new EqualsTo();
-            equalsTo.setLeftExpression(this.getAliasColumn(table));
-            equalsTo.setRightExpression(tenantLineMultiHandler.getTenantId());
-            if (where == null) {
-                return equalsTo;
-            }
-            if (where instanceof OrExpression) {
-                return new AndExpression(new Parenthesis(where), equalsTo);
-            } else {
-                return new AndExpression(where, equalsTo);
-            }
+            return new EqualsTo(getAliasColumn(table), tenantLineMultiHandler.getTenantId());
         }
     }
 }
