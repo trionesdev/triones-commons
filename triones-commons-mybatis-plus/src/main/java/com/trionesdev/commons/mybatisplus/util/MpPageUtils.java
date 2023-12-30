97700bb0162f6d73977e14fa2eb2aea80c490ff1
bane.shi <fengxiaotx@163.com>
2023-12-30 20:21:59 +0800

    feat: add page fun

--- triones-commons-mybatis-plus/src/main/java/com/trionesdev/commons/mybatisplus/util/MpPageUtils.java
+++ triones-commons-mybatis-plus/src/main/java/com/trionesdev/commons/mybatisplus/util/MpPageUtils.java
@@ -1,6 +1,8 @@
 package com.trionesdev.commons.mybatisplus.util;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.trionesdev.commons.core.page.PageCriteria;
 import com.trionesdev.commons.core.page.PageInfo;
 import com.trionesdev.commons.core.util.PageUtils;
 
@@ -25,5 +27,9 @@ public class MpPageUtils extends PageUtils {
         page.setTotal(pageInfo.getTotal());
         page.setRows(data);
         return page;
+    }
+
+    public static <T> IPage<T> page(PageCriteria criteria) {
+        return new Page<>(criteria.getPageNum(), criteria.getPageSize());
     }
 }
