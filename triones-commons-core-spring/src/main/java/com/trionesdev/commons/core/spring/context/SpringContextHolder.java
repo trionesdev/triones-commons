4597d564589f46ba0c104f0d5b2558d47d74fb79
Bane.Shi <fengxiaotx@163.com>
2024-05-31 18:47:00 +0800

    feat:  temporary save code

--- triones-commons-core-spring/src/main/java/com/trionesdev/commons/core/spring/context/SpringContextHolder.java
+++ triones-commons-core-spring/src/main/java/com/trionesdev/commons/core/spring/context/SpringContextHolder.java
@@ -1,16 +1,14 @@
 package com.trionesdev.commons.core.spring.context;
 
+import lombok.Getter;
 import org.springframework.context.ApplicationContext;
 
 public class SpringContextHolder {
     private SpringContextHolder() {
     }
 
+    @Getter
     private static ApplicationContext applicationContext;
-
-    public static ApplicationContext getApplicationContext() {
-        return applicationContext;
-    }
 
     public static void setApplicationContext(ApplicationContext applicationContext) {
         SpringContextHolder.applicationContext = applicationContext;
