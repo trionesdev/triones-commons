a2b9471e29eb430c98d6ac0917abb68dcf1c5d92
bane.shi <fengxiaotx@163.com>
2025-08-20 13:49:21 +0800

    feat: add enum option model

--- triones-commons-core/src/main/java/com/trionesdev/commons/model/EnumOption.java
+++ triones-commons-core/src/main/java/com/trionesdev/commons/model/EnumOption.java
@@ -5,14 +5,30 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Data
 @SuperBuilder
-@AllArgsConstructor
 @NoArgsConstructor
 public class EnumOption {
     private String value;
     private String label;
     private Map<String, String> extra;
+
+    public EnumOption(String value, String label) {
+        this.value = value;
+        this.label = label;
+    }
+
+    public EnumOption(String value, String label, Map<String, String> extra) {
+        this.value = value;
+        this.label = label;
+        this.extra = extra;
+    }
+
+    public static EnumOption of(String value, String label) {
+        return new EnumOption(value, label);
+    }
 }
