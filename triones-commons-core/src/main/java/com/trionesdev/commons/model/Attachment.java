2ee5b4c86a92b75c0b4653d2a735b62377aaf47a
bane.shi <fengxiaotx@163.com>
2025-10-09 23:24:51 +0800

    feat: add attachment id

--- triones-commons-core/src/main/java/com/trionesdev/commons/model/Attachment.java
+++ triones-commons-core/src/main/java/com/trionesdev/commons/model/Attachment.java
@@ -10,9 +10,10 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Attachment {
+    private String id;
     private String type;
     private String name;
-    private String imageUrl;
+    private String poster;
     private String url;
     private String extension;
 }
