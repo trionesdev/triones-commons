8456c3e65221dac6489b2bd02f637a1164c7fdf7
bane.shi <fengxiaotx@163.com>
2025-10-14 22:14:39 +0800

    feat: add attachment id

--- triones-commons-core/src/main/java/com/trionesdev/commons/model/Attachment.java
+++ triones-commons-core/src/main/java/com/trionesdev/commons/model/Attachment.java
@@ -1,10 +1,12 @@
 package com.trionesdev.commons.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @SuperBuilder
 @AllArgsConstructor
