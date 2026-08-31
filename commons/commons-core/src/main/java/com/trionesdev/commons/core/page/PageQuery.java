c22ff02d2cb8e6c4d6fc2fb800fc5ae53fa38065
bane.shi <fengxiaotx@163.com>
2026-08-31 11:09:55 +0800

    feat(page): 添加分页查询基础类
    - 新增 PageQuery 类作为分页查询的基础数据结构
    - 集成 Lombok 注解简化代码开发
    - 提供链式调用支持提升开发效率
    - 支持构建器模式便于对象创建
    - 包含页码和页面大小两个核心属性

---
+++ commons/commons-core/src/main/java/com/trionesdev/commons/core/page/PageQuery.java
@@ -0,0 +1,17 @@
+package com.trionesdev.commons.core.page;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@Accessors(chain = true)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageQuery {
+    private Integer pageNum;
+    private Integer pageSize;
+}
